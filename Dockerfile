91b8257068be55d7830fdeb945209af586cb9da9
abolfazl1381 <abolfazlandalib@gmail.com>
2024-10-08 16:56:40 +0330

    add nginx reverse proxy

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:3
+FROM python:3.8
 RUN pip install rembg[gpu,cli]
 WORKDIR "/root"
 
 EXPOSE 7000
 VOLUME ["/root/.u2net"]
-ENTRYPOINT [ "./entrypoint.sh" ]
+#ENTRYPOINT [ "entrypoint.sh" ]
--- test_containers.py
+++ test_containers.py
@@ -11,3 +11,10 @@ def test_cadvisor_running():
 def test_prometheus_running():
     response = requests.get('http://localhost:9090')
     assert response.status_code == 200
+
+def test_nginx():
+    response = requests.get('http://localhost:80/rembg')
+    assert response.status_code == 200
+
+    response = requests.get('http://localhost:80/grafana')
+    assert response.status_code == 200
