c700e48fc3fd8384da7480647e4fcf10f61eb413
abolfazl1381 <abolfazlandalib@gmail.com>
2024-10-13 10:03:10 +0330

    add nginx prometheus exporter

--- nvidia_container.sh
+++ nvidia_container.sh
@@ -1,3 +1,4 @@
+
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
@@ -7,4 +8,4 @@ sudo apt-get update &&
 sudo apt-get install -y nvidia-container-toolkit &&
 sudo systemctl restart docker &&
 sudo docker pull nvidia/cuda:11.0.3-base-ubuntu20.04 &&
-sudo docker run --rm --gpus all nvidia/cuda:11.0.3-base-ubuntu20.04 nvidia-smi
+sudo docker run --rm --gpus all nvidia/cuda:11.0.3-base-ubuntu20.04 nvidia-smi &&
